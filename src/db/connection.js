68cb53c64e09ea428c239d3bffdcc2bc30cc9829
Eduardo <eduardomail77@gmail.com>
2026-08-21 11:36:49 -0300

    Add DATABASE_URL logging

--- src/db/connection.js
+++ src/db/connection.js
@@ -5,6 +5,8 @@ dotenv.config();
 
 const { Pool } = pg;
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL ? process.env.DATABASE_URL.substring(0, 50) + '...' : 'undefined');
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
